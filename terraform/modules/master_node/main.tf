52a64edc541be5f28a90a025e50e762ebad8fb71
Charlie Egan <charlieegan3@users.noreply.github.com>
2017-05-19 14:27:29 +0100

    Add master EBS devices

--- terraform/modules/master_node/main.tf
+++ terraform/modules/master_node/main.tf
@@ -57,3 +57,31 @@ resource "aws_launch_configuration" "master" {
     create_before_destroy = true
   }
 }
+
+resource "aws_ebs_volume" "main" {
+  availability_zone = "${var.availability_zone}"
+  size              = 20
+  type              = "gp2"
+
+  tags {
+    Name          = "${var.cluster_name}-master-${var.number}-main"
+    type          = "main"
+    kubernetes    = true
+    cluster_name  = "${var.cluster_name}"
+    master_number = "${var.number}"
+  }
+}
+
+resource "aws_ebs_volume" "events" {
+  availability_zone = "${var.availability_zone}"
+  size              = 20
+  type              = "gp2"
+
+  tags {
+    Name          = "${var.cluster_name}-master-${var.number}-events"
+    type          = "events"
+    kubernetes    = true
+    cluster_name  = "${var.cluster_name}"
+    master_number = "${var.number}"
+  }
+}
